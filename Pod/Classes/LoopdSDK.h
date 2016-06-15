cb5249baf3dd2014bfdc0c8164471f3159cd1f30
Derrick <ccsderrick@gmail.com>
2016-06-15 15:17:49 +0800

    fix

---
+++ Pod/Classes/LoopdSDK.h
@@ -0,0 +1,14 @@
+//
+//  LoopdBadgeSDK.h
+//  LoopdBadgeSDK
+//
+//  Created by Derrick Chao on 2015/9/3.
+//  Copyright (c) 2015年 Loopd. All rights reserved.
+//
+
+#import <LoopdSDK/LCBadge.h>
+#import <LoopdSDK/LCBadgeManager.h.h>
+#import <LoopdSDK/LCContactExchangeManager.h.h>
+#import <LoopdSDK/LCScanningConfig.h.h>
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoopdBadgeSDK/PublicHeader.h>
